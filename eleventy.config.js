3fc3e27e3720fc49b88ab44516a407cc42a2873c
Amber Cronin <amber@amber.vision>
2023-05-14 19:59:49 +0100

    Add drafts support

--- eleventy.config.js
+++ eleventy.config.js
@@ -44,6 +44,28 @@ module.exports = function (eleventyConfig) {
     return arr;
   });
 
+  eleventyConfig.addGlobalData("eleventyComputed.permalink", function () {
+    return (data) => {
+      if (data.draft && !process.env.BUILD_DRAFTS) return false;
+      return data.permalink;
+    };
+  });
+
+  eleventyConfig.addGlobalData(
+    "eleventyComputed.eleventyExcludeFromCollections",
+    function () {
+      return (data) => {
+        if (data.draft && !process.env.BUILD_DRAFTS) return true;
+        return data.eleventyExcludeFromCollections;
+      };
+    }
+  );
+
+  eleventyConfig.on("eleventy.before", ({ runMode }) => {
+    if (runMode === "serve" || runMode === "watch")
+      process.env.BUILD_DRAFTS = true;
+  });
+
   return {
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
