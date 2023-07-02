2b7d864d24a3da1c86d2d7c63212268cc0a213bd
Amber Cronin <amber@amber.vision>
2023-07-02 20:46:21 +0100

    Run pre-commit hooks

--- data/meta.js
+++ data/meta.js
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 module.exports = {
   siteName: "amber.vision",
   siteDescription: "My personal web zone",
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
 const csso = require("csso");
