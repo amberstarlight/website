dcbb204e499dbdba40287836d1b1c656da82942a
Amber Cronin <amber@amber.vision>
2025-09-25 18:08:45 +0100

    Remove drafts configuration

--- eleventy.config.js
+++ eleventy.config.js
@@ -72,28 +72,6 @@ export default function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/assets");
   eleventyConfig.addWatchTarget("./util");
 
-  eleventyConfig.addGlobalData("eleventyComputed.permalink", function () {
-    return (data) => {
-      if (data.draft && !process.env.BUILD_DRAFTS) return false;
-      return data.permalink;
-    };
-  });
-
-  eleventyConfig.addGlobalData(
-    "eleventyComputed.eleventyExcludeFromCollections",
-    function () {
-      return (data) => {
-        if (data.draft && !process.env.BUILD_DRAFTS) return true;
-        return data.eleventyExcludeFromCollections;
-      };
-    },
-  );
-
-  eleventyConfig.on("eleventy.before", ({ runMode }) => {
-    if (runMode === "serve" || runMode === "watch")
-      process.env.BUILD_DRAFTS = true;
-  });
-
   eleventyConfig.on(
     "eleventy.after",
     ({ dir, results, runMode, outputMode }) => {
