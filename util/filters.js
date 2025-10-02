6ad9659b18ffc0da0ead40e20c955b07467494a0
Amber Cronin <amber@amber.vision>
2025-10-02 12:06:54 +0100

    Add initial footnote system
    Credit to Kevin Loughead[0]
    
    [0]: https://www.kevinloughead.com/blog/footnotes/

--- util/filters.js
+++ util/filters.js
@@ -2,6 +2,21 @@
 
 const tagFilter = ["all", "now", "post"];
 
+function parseFootnotes(htmlString) {
+  const footnoteRegex = /\[\^([1-9])\]/g;
+  const footnotes = new Set();
+  const pageURL = this.ctx.page.url;
+
+  htmlString = htmlString.replace(footnoteRegex, (match, $1) => {
+    if (!footnotes.has($1)) {
+      footnotes.add($1);
+      return `<a id="backlink-${$1}" href="${pageURL}#footnote-${$1}">[${$1}]</a>`;
+    }
+    return `<a id="footnote-${$1}" href="${pageURL}#backlink-${$1}">[${$1}]</a>`;
+  });
+  return htmlString;
+}
+
 export default function (eleventyConfig) {
   eleventyConfig.addFilter("alphabeticSort", (arr) =>
     arr.sort((a, b) => (a > b ? 1 : -1)),
@@ -19,4 +34,6 @@ export default function (eleventyConfig) {
   eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
     return (tags || []).filter((tag) => tagFilter.indexOf(tag) === -1);
   });
+
+  eleventyConfig.addFilter("parseFootnotes", parseFootnotes);
 }
