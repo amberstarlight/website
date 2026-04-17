75a355c53a3cbf90edc5ab448537d7d646f8bfe8
Amber Cronin <amber@amber.vision>
2026-04-18 00:20:53 +0100

    Update note system

--- util/shortcodes.js
+++ util/shortcodes.js
@@ -1,5 +1,18 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+function dateTimeSlug(date) {
+  const now = new Date(date);
+
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  const day = now.getDate();
+  const hour = now.getHours();
+  const mins = now.getMinutes();
+
+  const dateString = `${year}-${month}-${day}-${hour}-${mins}`;
+  return dateString;
+}
+
 export default function (eleventyConfig) {
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
@@ -17,4 +30,19 @@ export default function (eleventyConfig) {
         year: "numeric",
       })}`,
   );
+
+  eleventyConfig.addShortcode(
+    "postedTime",
+    (date) =>
+      `${new Date(date).toLocaleString("en-GB", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        timeZoneName: "short",
+      })}`,
+  );
+
+  eleventyConfig.addShortcode("dateSlug", (date) => dateTimeSlug(date));
 }
